4d9595f861a3999acd43dc42bf7e9913e9488258
henriquelbk <loubackhenrique123@gmail.com>
2024-08-29 22:51:45 -0300

    Footer component created

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client"
 
 import Head from "next/head";
+import Footer from "./components/Footer";
 
 export default function Home() {
 
@@ -34,13 +35,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <p className="col-md-4 mb-0 text-body-secondary">&copy; 2024 Webbb3, Inc</p>
-          <ul className="nav col-md-4 justify-content-end">
-            <li className="nav-item"><a href="/" className="nav-link px-2 text-body-secondary">Home</a></li>
-            <li className="nav-item"><a href="/about" className="nav-link px-2 text-body-secondary">About</a></li>
-          </ul>
-        </footer>
+        <Footer />
       </div>
     </>
   );
